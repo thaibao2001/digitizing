f34673a052b500562ce6dc628d491707ef2ed847
Le Minh <le.minh011100@gmail.com>
2022-04-10 16:37:07 +0700

    updated: entity internship

---
+++ src/app/main/leader/entities/internship.ts
@@ -0,0 +1,10 @@
+export class Internship {
+  public internship_id_rcd: string;
+  public academic_year: string;
+  public semester: number;
+  public active_flag: number;
+  public created_by_user_id: string;
+  public created_date_time: Date;
+  public lu_updated: Date;
+  public lu_user_id: string;
+}
